386cca9cae9084849756c18a400f0202e99365f5
wdd <296151852@qq.com>
2022-05-16 21:45:14 +0800

    1

--- soft/wap/src/pages/novel/weapon/weapon.vue
+++ soft/wap/src/pages/novel/weapon/weapon.vue
@@ -1,30 +1,18 @@
 <template>
-  <div class="home">
-    <canvas id="canvas" />
+  <div class="weapon-main">
   </div>
 </template>
 
 <script>
-import { init, start } from "../../../utiles/canvas";
 export default {
   data() {
     return {
-      ctx: {},
     };
   },
   mounted() {
-    this.initCanvas();
   },
   methods: {
-    initCanvas() {
-      console.log("初始化canvas");
-      const canvas = document.querySelector("#canvas");
-      canvas.width = document.documentElement.clientWidth;
-      canvas.height = document.documentElement.clientHeight;
-      this.ctx = canvas.getContext("2d");
-      init(this.ctx, canvas.width, canvas.height);
-      start();
-    },
+   
   },
 };
 </script>
