e5802c616b21e1bf1313c141102a6e9a52146b8b
wdd <296151852@qq.com>
2022-05-18 21:38:54 +0800

    1

--- soft/wap/src/pages/novel/fatalism/fatalism.vue
+++ soft/wap/src/pages/novel/fatalism/fatalism.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="fatalism-main">
     <div class="fatalism-model">
-      <div v-for="(item, index) in list" :key="index" 
-        :style="{ top: item.top+'px', left: item.left + 'px' }">
-        <div>
-          <span>{{item.name}}</span>
-          <h2>{{item.title}}</h2>
-        </div>
+      <div class="round" 
+        v-for="(item, index) in list" 
+        :key="index" 
+        :style="{top:item.top+'%',left:item.left+'%'}"
+        @click="goPage(item)"
+      ></div>
+      <div class="info" 
+        v-for="(item, index) in list" 
+        :key="'id'+index" 
+        :style="{top:item.topTip+'%',left:item.leftTip+'%'}"
+        @click="goPage(item)"
+      >
+        {{item.name}}
       </div>
     </div>
-    <div class="fatalism-bg"></div>
   </div>
 </template>
 
@@ -19,30 +25,63 @@ export default {
     return {
       list: [
         {
-          top: 100,
-          left: 100,
+          top: 10,
+          left: 10,
+          topTip: 10-6,
+          leftTip: 10-2,
           title: '少室山',
-          name: '初试江湖'
+          name: '初试江湖',
+          code: '001'
         },
         {
-          top: 100,
-          left: 300,
-          title: '少室山',
-          name: '初试江湖'
+          top: 20,
+          left: 30,
+          topTip: 20-6,
+          leftTip: 30-2,
+          title: '紫禁城',
+          name: '香楼听曲',
+          code: '002'
         },
         {
-          top: 100,
-          left: 800,
-          title: '少室山',
-          name: '初试江湖'
+          top: 30,
+          left: 10,
+          topTip: 30-6,
+          leftTip: 10-2,
+          title: '小重山',
+          name: '邪剑之争',
+          code: '003'
+        },
+        {
+          top: 50,
+          left: 50,
+          topTip: 50-6,
+          leftTip: 50-2,
+          title: '紫禁城',
+          name: '名剑山庄论剑',
+          code: '004'
+        },
+        {
+          top: 70,
+          left: 50,
+          topTip: 70-6,
+          leftTip: 50-2,
+          title: '小重山',
+          name: '风花雪月',
+          code: '003'
         }
       ]
     };
   },
   mounted() {
+    // this.$nextTick(() => { 
+    //   console.log(document.documentElement.clientHeight);
+    // });
   },
   methods: {
-   
+   // 跳转
+   goPage(data) {
+     console.log(data)
+   }
   },
 };
 </script>
@@ -51,36 +90,48 @@ export default {
   .fatalism-main {
     font-size: 14px;
     .fatalism-model {
-      // position: absolute;
-      // height: 100%;
-      // width: 100%;
-      overflow-x: auto;
-      // overflow-y: hidden;
-      // z-index: 2;
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background: linear-gradient(to top, #3932ff, #5d84dc);
       >div {
         position: absolute;
-        padding: 8px;
-        box-sizing: border-box;
-        z-index: 3;
-        >div {
-          position: relative;
-          >span {
-            padding: 4px;
-            box-sizing: border-box;
-          }
-          >h2 {
+        background-color: #000;
+        color: #fff;
+        cursor: pointer;
+        &.round {
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          background-color: #fff;
+          animation: toshow 2s;
+        }
+        &.info {
+          padding: 8px;
+          box-sizing: border-box;
+          border-radius: 4px;
+          animation: toshow 2s;
+          &::after {
+            content: '';
             position: absolute;
-            font-size: 16px;
+            bottom: -9px;
+            left: 12px;
+            border: 5px solid #000;
+            border-top-color: #000;
+            border-bottom-color: transparent;
+            border-left-color: transparent;
+            border-right-color: transparent;
           }
         }
       }
     }
-    .fatalism-bg {
-      position: fixed;
-      height: 100%;
-      width: 100%;
-      background: linear-gradient(to bottom, #d87af8, #3932ff);
-      z-index: 1;
+  }
+  @keyframes toshow {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
     }
   }
 </style>
