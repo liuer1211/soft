dd129c0fb51ff418e73abb6f83a8ebe6ce73ee38
wdd <296151852@qq.com>
2022-05-21 17:28:26 +0800

    1

--- soft/wap/src/pages/novel/weapon/weapon.vue
+++ soft/wap/src/pages/novel/weapon/weapon.vue
@@ -2,15 +2,24 @@
   <div class="weapon-main">
     <div class="swiper">
       <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <img src="../../../assets/images/imgmodel/001.png" />
-          <div>
-            <div>黑曜</div>
-            <div>邪剑黑曜，旷世奇兵</div>
+        <div class="swiper-slide" v-for="(item, index) in list" :key="index">
+          <div @click.stop="item.active = !item.active" :class="{active:item.active}">
+            <div>
+              <img :src="getImg(item.imgs)" />
+              <div>
+                <div>{{item.name}}</div>
+              </div>
+            </div>
+            <div>
+              <div>{{item.des}}</div>
+            </div>
+            <!-- <i>
+              icon-exchangerate
+            </i> -->
+            <div>
+              <i class="iconfont icon-exchangerate"></i>
+            </div>
           </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="../../../assets/images/people/y.jpg" />
         </div>
       </div>
       <div class="swiper-pagination"></div>
@@ -25,22 +34,44 @@ import "swiper/swiper-bundle.min.css";
 export default {
   data() {
     return {
+      active: false,
+      list: [
+        {
+          name: '黑曜', 
+          imgs: 'imgmodel/001.png', 
+          des: '邪剑黑曜，旷世奇兵', 
+          active: false
+        },
+        {
+          name: '银枪', 
+          imgs: 'people/y.jpg', 
+          des: '百鸟朝凤，惊艳一枪', 
+          active: false
+        }
+      ]
     };
   },
   mounted() {
-    this.getInitDate();  // 悬浮------------
+    // 初始数据
+    this.getInitDate();
   },
   methods: {
+    // 初始数据
     getInitDate() {
       this.$nextTick(() => {
         let swiper = new Swiper(".swiper", {
-          effect: "flip",
+          effect: "coverflow",
           grabCursor: true,
           pagination: {
             el: ".swiper-pagination",
           },
         });
       });
+    },
+    // 动态拼接图片地址
+    getImg(data) {
+      let img = require(`../../../assets/images/${data}`)
+      return img;
     }
   },
 };
@@ -51,38 +82,96 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
-    padding: 20px;
     box-sizing: border-box;
     background: linear-gradient(to top, #dfdb04, #f0f5b2);
     .swiper {
+      width: 100%;
+      height: 100%;
+      padding: 20px 20px 50px;
+      box-sizing: border-box;
       .swiper-wrapper {
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height: 100%;
         .swiper-slide {
           position: relative;
-          img {
+          >div {
+            position: relative;
             width: 100%;
             height: 100%;
-            object-fit: cover;
-            display: block;
+            transition: 1s;
             border-radius: 4px;
-            box-shadow: 1px 1px 1px 1px #efe58e;
-          }
-          >div {
-            position:absolute;
-            top: 16px;
-            right: 56px;
-            bottom: 16px;
-            padding-left: 16px;
-            box-sizing: border-box;
-            writing-mode: vertical-rl;
-            color: #fff;
-            transition: .5s;
-            transform: scale(.8);
+            overflow: hidden;
+            cursor: pointer;
+            &.active {
+              transform: rotateY(180deg);
+              >div:nth-child(1) {
+                display: none;
+              }
+              >div:nth-child(2) {
+                display: block;
+              }
+              >div:nth-child(3) {
+                left: initial;
+                right: 0;
+              }
+            }
+            >div:nth-child(1) {
+              width: 100%;
+              height: 100%;
+              display: block;
+              background-color: #dab22f;
+              img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                display: block;
+                border-radius: 4px;
+                box-shadow: 1px 1px 1px 1px #efe58e;
+              }
+              >div {
+                position:absolute;
+                top: 20px;
+                right: 20px;
+                writing-mode: vertical-rl;
+                color: #fff;
+              }
+            }
+            >div:nth-child(2) {
+              width: 100%;
+              height: 100%;
+              transform: rotateY(540deg);
+              background-color: #dab22f;
+              display: none;
+              >div {
+                position:absolute;
+                top: 20px;
+                right: 20px;
+                writing-mode: vertical-rl;
+                color: #fff;
+              }
+            }
+            >div:nth-child(3) {
+              position: absolute;
+              left: 0;
+              top: 0;
+              width: 40px;
+              height: 40px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              i {
+                color: #fff;
+                font-size: 20px;
+              }
+            }
           }
         }
       }
     }
     .swiper-pagination-bullets.swiper-pagination-horizontal{
-      bottom: -28px;
+      bottom: 18px;
     }
   }
 </style>
