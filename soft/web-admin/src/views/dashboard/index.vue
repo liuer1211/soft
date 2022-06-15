b837330765e9c8bdb8509358b5b26e80a5ced8df
wdd <296151852@qq.com>
2022-06-15 21:24:23 +0800

    1

--- soft/web-admin/src/views/dashboard/index.vue
+++ soft/web-admin/src/views/dashboard/index.vue
@@ -38,6 +38,8 @@
         </el-col>
       </el-row>
     </div>
+
+    <TinymceEditor />
   </div>
 </template>
 
@@ -45,7 +47,7 @@
 import { mapGetters } from 'vuex';
 import axios from 'axios';
 import AMap from "AMap";
-
+import TinymceEditor from  "../../components/editor/TinymceEditor.vue"
 export default {
   name: 'Dashboard',
   computed: {
@@ -53,11 +55,34 @@ export default {
       'name'
     ])
   },
+  components: {
+    TinymceEditor,
+  },
   data() {
     return {
       citys: {}, // 城市
       weather: {}, // 天气
       city: '', // 城市
+      nit: {
+
+        // 组件
+
+        plugins:
+
+          ' lists image colorpicker textcolor wordcount contextmenu autoresize',
+
+        // 工具栏
+
+        toolbar:
+
+          'bold italic underline strikethrough | fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent  | undo redo | link unlink image  | removeformat ',
+
+        branding: false,
+
+        min_height: 300
+
+      }
+
     }
   },
   created() {
