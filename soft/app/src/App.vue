f1ee77129e051cbec6177382bde7535c7bf8b677
wdd <wdd296151852@163.com>
2023-06-12 19:58:56 +0800

    1

---
+++ soft/app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="app">
+    <!-- 打包app,直接跳转vue项目 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted(){
+    window.location.href="http://180.76.106.221"
+  }
+}
+</script>
+
+<style>
+  .app{
+    background: #000;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+  }
+</style>
